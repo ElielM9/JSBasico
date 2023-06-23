c1382485c773cee1f381a92a8d2817fcfa260eef
ElielM9 <elielm291@gmail.com>
2023-06-22 19:42:32 -0600

    Seccion 1 y 2 terminadas

---
+++ 02-TiposDatos/js/01variables.js
@@ -0,0 +1,18 @@
+// Variables en JS
+
+// Definicion de variables
+var numero2 = 20; // No se recomienda el uso de var
+let numero1 = 10; 
+const numero3 = 30; // Es una constante, debe iniciar con un valor y no se puede reasignar. 
+
+
+console.log(numero1);
+
+// Reglas de definicion de variables
+/*
+    1- Utilizar letras y simbolos desde el inicio pero no puede iniciar con numeros
+*/
+
+let usuario;
+let $producto;
+// let 1numero;
